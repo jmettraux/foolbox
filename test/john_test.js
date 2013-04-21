1c0bd3cb64730cae04e45e4b1c6ab1cd6224c102
John Mettraux <jmettraux@gmail.com>
2013-04-21 14:37:47 +0900

    reorganize test/

--- test/john_test.js
+++ test/john_test.js
@@ -9,64 +9,64 @@
 //
 // testing John.parse(s)
 
-function j_assert(source, expected, message) {
+function pa(source, expected, message) {
 
   deepEqual(John.parse(source), expected, message);
 }
 
 test('John.parse(o) atomic values', function() {
 
-  j_assert('"oh yeah"', 'oh yeah');
-  j_assert('"わしらの電車"', 'わしらの電車');
-  j_assert('"a:b"', 'a:b');
+  pa('"oh yeah"', 'oh yeah');
+  pa('"わしらの電車"', 'わしらの電車');
+  pa('"a:b"', 'a:b');
 
-  j_assert('1', 1);
-  j_assert('1.2', 1.2);
-  j_assert('1.2e10', 1.2e10);
-  j_assert('-1', -1);
+  pa('1', 1);
+  pa('1.2', 1.2);
+  pa('1.2e10', 1.2e10);
+  pa('-1', -1);
 
-  j_assert('true', true);
-  j_assert('false', false);
+  pa('true', true);
+  pa('false', false);
 
-  j_assert('null', null);
+  pa('null', null);
 });
 
 test('John.parse(o) single quoted strings', function() {
 
-  j_assert("'de nada'", 'de nada');
-  j_assert("'わしらの電車'", 'わしらの電車');
-  j_assert("'she said \"too bad!\"'", 'she said "too bad!"');
+  pa("'de nada'", 'de nada');
+  pa("'わしらの電車'", 'わしらの電車');
+  pa("'she said \"too bad!\"'", 'she said "too bad!"');
 });
 
 test('John.parse(o) standalone strings', function() {
 
-  j_assert("炊飯器", '炊飯器');
-  //j_assert("de nada", 'de nada');
+  pa("炊飯器", '炊飯器');
+  //pa("de nada", 'de nada');
 });
 
 test('John.parse(o) vanilla arrays', function() {
 
-  j_assert('[]', []);
-  j_assert('[ 1, 2, "trois" ]', [ 1, 2, 'trois' ]);
+  pa('[]', []);
+  pa('[ 1, 2, "trois" ]', [ 1, 2, 'trois' ]);
 });
 
 test('John.parse(o) vanilla objects', function() {
 
-  j_assert('{}', {});
-  j_assert('{ "a": 1 }', { a: 1 });
+  pa('{}', {});
+  pa('{ "a": 1 }', { a: 1 });
 });
 
 test('John.parse(o) relaxed arrays', function() {
 
-  j_assert("[ 1, 2, 'trois' ]", [ 1, 2, 'trois' ]);
-  j_assert("[ 1 2 'trois' ]", [ 1, 2, 'trois' ]);
+  pa("[ 1, 2, 'trois' ]", [ 1, 2, 'trois' ]);
+  pa("[ 1 2 'trois' ]", [ 1, 2, 'trois' ]);
 });
 
 test('John.parse(o) relaxed objects', function() {
 
-  j_assert("{ a: 0, b: 1 }", { a: 0, b: 1 });
-  j_assert("{ a 0, b 1 }", { a: 0, b: 1 });
-  j_assert("{ a 0 b 1 }", { a: 0, b: 1 });
+  pa("{ a: 0, b: 1 }", { a: 0, b: 1 });
+  pa("{ a 0, b 1 }", { a: 0, b: 1 });
+  pa("{ a 0 b 1 }", { a: 0, b: 1 });
 });
 
 test('John.parse(o) (readme)', function() {
@@ -81,20 +81,20 @@ test('John.parse(o) (readme)', function() {
     { a: 'alpha', b: 'bra vo', c: [ 'delta', 'echo' ] });
 });
 
-function t(s, expected, message) {
+function ta(s, expected, message) {
 
   deepEqual(John.t([], s), expected, message);
 }
 
 test('John tokenization', function() {
 
-  t(
+  ta(
     "'a string'",
     [ "a string" ]);
-  t(
+  ta(
     "{ a: b, c: d }",
     [ 0, "a", "b", "c", "d", 1 ]);
-  t(
+  ta(
     "{ 'a': [ 1, 2, 'trois' ], c: d }",
     [ 0, "a", 2, "1", "2", "trois", 3, "c", "d", 1 ]);
 });
@@ -103,30 +103,30 @@ test('John tokenization', function() {
 //
 // testing John.stringify(o)
 
-function s_assert(o, expected, message) {
+function sa(o, expected, message) {
 
   equal(John.stringify(o), expected, message);
 }
 
 test('John.stringify(o)', function() {
 
-  s_assert(null, 'null');
+  sa(null, 'null');
 
-  s_assert('a', 'a');
-  s_assert('a b', '"a b"');
+  sa('a', 'a');
+  sa('a b', '"a b"');
 
-  s_assert(1, '1');
+  sa(1, '1');
 
-  s_assert([ 1, 2 ], '[ 1, 2 ]');
+  sa([ 1, 2 ], '[ 1, 2 ]');
 
-  s_assert({ a: 0, b: 1 }, '{ a: 0, b: 1 }');
-  s_assert({ a: 'apple', b: 'pie' }, '{ a: apple, b: pie }');
-  s_assert({ a: null, b: 'burger' }, '{ a, b: burger }');
+  sa({ a: 0, b: 1 }, '{ a: 0, b: 1 }');
+  sa({ a: 'apple', b: 'pie' }, '{ a: apple, b: pie }');
+  sa({ a: null, b: 'burger' }, '{ a, b: burger }');
 });
 
 test('John.stringify(o) (readme)', function() {
 
-  s_assert(
+  sa(
     { a: 0, b: "trois", 'c': true, d: [ 'alpha', 'bravo', 'charly' ] },
     '{ a: 0, b: trois, c: true, d: [ alpha, bravo, charly ] }'
   );
